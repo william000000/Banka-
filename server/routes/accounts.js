9bd692fc7ab44312a93df95b9253c19802cb616d
william000000 <40672143+william000000@users.noreply.github.com>
2019-04-10 15:40:36 +0200

    Revert "[Feature #165242394] Debit User Account"

--- server/routes/accounts.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import AccountController from "../controllers/accounts"; 
-const router=express.Router();
-
-router.post("/",AccountController.create);
-router.patch("/:id",AccountController.activate);
-router.post("/:accountNumber/debit",AccountController.debit);
-
-export default router;
