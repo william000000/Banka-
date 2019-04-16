517e1af33c42fe57722cb16b18cad2a4f3a059f6
William000000 <sugiraw@gmail.com>
2019-04-16 15:14:22 +0200

    [bug #165377810] changing UI

---
+++ UI/js/model.js
@@ -0,0 +1,11 @@
+function display() {
+    document.getElementById("accountList").style.opacity=0.2;
+    document.getElementById("modalTable").style.display = 'block';
+  }
+  function disappear() {
+    document.getElementById("accountList").style.opacity=1;
+    document.getElementById("modalTable").style.display = 'none';
+  }
+   const close= document.getElementById("close");
+
+close.addEventListener('click', disappear);
